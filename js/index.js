de4fc04b202a79993c6233f6616ade61202bd033
vladislav77 <vladperyazev14@gmail.com>
2023-02-21 13:44:39 +0300

    feat: add ability to edit task, render task list

--- js/index.js
+++ js/index.js
@@ -1,35 +1,75 @@
 "use strict";
+const tasksList = [
+  'полить цветы', 'пройти туториал по реакту', 'помыть посуду'
+];
+
 const createNewTaskFormElement = document.querySelector('.block__form');
 const createNewTaskInputElement = createNewTaskFormElement.querySelector('.block__input-text');
 
 const tasksListElement = document.querySelector('.main__tasks');
 
-createNewTaskFormElement.addEventListener('submit', (event) => {
+
+let editableElement = null;
+
+function handleEdit() {
+  const taskDescriptionElement = editableElement.querySelector('.task__description');
+  taskDescriptionElement.textContent = createNewTaskInputElement.value;
+}
+
+function handleFormSubmit(event) {
   event.preventDefault();
+
   const taskDecription = createNewTaskInputElement.value;
-  const taskElement = createTaskElement(taskDecription);
-  tasksListElement.prepend(taskElement);
-});
+  if ( editableElement !== null) {
+    handleEdit();
+  } else {
+    renderTask(taskDecription);
+  }
+  createNewTaskInputElement.value = '';
+}
 
-
+createNewTaskFormElement.addEventListener('submit', handleFormSubmit);
 
 const taskTemplateContent = document.querySelector('#task-template').content;
+
+function handleDeleteButtonClick(event) {
+  const taskElement = event.target.closest('.tasks__task');
+  taskElement.remove();
+}
 
 function createTaskElement(taskDescription) {
   const taskBlockElement = taskTemplateContent.cloneNode(true);
 
   const taskCheckBoxElement = taskBlockElement.querySelector('.block__input-checkbox');
   const taskDescriptionElement = taskBlockElement.querySelector('.task__description');
+  function handleDescriptionClick(event) {
+    createNewTaskInputElement.focus();
+    createNewTaskInputElement.value = taskDescriptionElement.textContent;
+    editableElement = event.target.closest('.tasks__task');
+  }
+  taskDescriptionElement.addEventListener('click', handleDescriptionClick)
   const taskDeleteButtonElement = taskBlockElement.querySelector('.task__button');
   taskDescriptionElement.textContent = taskDescription;
-  taskDeleteButtonElement.addEventListener('click', (event) => {
-    const taskElement = event.target.closest('.tasks__task');
-    taskElement.remove();
-  });
-
+  taskDeleteButtonElement.addEventListener('click', handleDeleteButtonClick);
 
   return taskBlockElement;
 }
+
+function renderTask(taskDecription) {
+  const taskElement = createTaskElement(taskDecription);
+  tasksListElement.prepend(taskElement);
+}
+
+function renderTasks(tasksList) {
+  tasksList.forEach(renderTask);
+}
+
+renderTasks(tasksList);
+
+
+
+
+
 
 
 
